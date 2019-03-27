a24a7ad7b3530f5b8fb39b1750ff5320e752778b
Justin Pacquing <pacqqu@gmail.com>
2019-03-27 11:26:07 -0400

    added login

--- api/routes/user.js
+++ api/routes/user.js
@@ -4,7 +4,7 @@ var router = express.Router();
 var SpotifyWebApi = require('spotify-web-api-node');
 var scopes = ['user-read-private', 'user-read-email', 'user-read-birthdate', 'user-top-read', 'user-library-read',
 'playlist-modify-private', 'playlist-read-private', 'playlist-modify-public','user-read-recently-played'],
-  state = 'desplaycito-state';
+  state = 'spotification-state';
 var configSpotify = require('./config-spotify');
 var spotifyApi = new SpotifyWebApi(configSpotify);
 
@@ -22,6 +22,7 @@ const url = 'mongodb://localhost:27017';
 const dbName = 'spotification';
 // Create a new MongoClient
 const client = new MongoClient(url);
+
 let db;
 client.connect(function(err) {
   console.log("Connected successfully to server");
@@ -46,21 +47,30 @@ const insertDocument = function(db, collectionName, doc, callback ){
   const collection = db.collection(collectionName);
   // Find some documents
   collection.insertOne(doc, (err, results) => {
-    console.log("Found the following records");
+    console.log("Inserted the following document");
     console.log(results);
     callback(results);
   });
 };
 
+
 /* Register User */
+//TODO:
+// - Hash Password
+// - Add rest of user data into user documents
+// -- Generate spotify url for auth
 router.post('/', function(req, res, next) {
   var username = req.body.username;
-  console.log(username);
   var password = req.body.password;
-  console.log(password);
   findDocuments(db, 'users', {'username': username}, (results) => {
     if ( results.length == 0  || !(results) ) {
-      insertDocument(db, 'users', {'username': username}, (results) => {
+      let userDoc = {
+        'username': username,
+        'password': password,
+        'full_name': req.body.fullName,
+        'spotify_auth_url': spotifyApi.createAuthorizeURL(scopes, state)
+      }
+      insertDocument(db, 'users', userDoc, (results) => {
         jwt.sign({'username': username}, jwtSecret , { expiresIn: '1d' },(err, token) => {
           if(err) { console.log(err) }
           res.send(token);
@@ -73,23 +83,23 @@ router.post('/', function(req, res, next) {
   })
 });
 
-//Login
-router.post('/', function(req, res, next) {
+
+/* Login User */
+//TODO:
+router.post('/login', function(req, res, next) {
   var username = req.body.username;
-  console.log(username);
+  //console.log(username);
   var password = req.body.password;
-  console.log(password);
-  findDocuments(db, 'users', {'username': username}, (results) => {
+  //console.log(password);
+  findDocuments(db, 'users', {'username': username, 'password': password}, (results) => {
     if ( results.length == 0  || !(results) ) {
-      insertDocument(db, 'users', {'username': username}, (results) => {
-        jwt.sign({'username': username}, jwtSecret , { expiresIn: '1d' },(err, token) => {
-          if(err) { console.log(err) }
-          res.send(token);
-        });
-      })
+      res.send("User doesn't Exists");
     }
     else {
-      res.send('User Already Exists');
+      jwt.sign({'username': username}, jwtSecret , { expiresIn: '1d' },(err, token) => {
+        if(err) { console.log(err) }
+        res.send(token);
+      });
     }
   })
 });
