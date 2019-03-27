e06df0438d5202969e2abfdcda6ec6765086c2e7
Justin Pacquing <pacqqu@gmail.com>
2019-03-27 10:53:51 -0400

    implemented jwt in register endpoint

--- api/routes/user.js
+++ api/routes/user.js
@@ -1,14 +1,17 @@
 var express = require('express');
 var router = express.Router();
+
 var SpotifyWebApi = require('spotify-web-api-node');
 var scopes = ['user-read-private', 'user-read-email', 'user-read-birthdate', 'user-top-read', 'user-library-read',
 'playlist-modify-private', 'playlist-read-private', 'playlist-modify-public','user-read-recently-played'],
   state = 'desplaycito-state';
-var config-spotify = require('./config-spotify');
+var configSpotify = require('./config-spotify');
+var spotifyApi = new SpotifyWebApi(configSpotify);
 
-var spotifyApi = new SpotifyWebApi(config-spotify);
 
 const jwt = require('jsonwebtoken');
+var jwtFunctions = require('./jwt');
+var jwtSecret = require('./config-jwt');
 
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
@@ -23,6 +26,8 @@ let db;
 client.connect(function(err) {
   console.log("Connected successfully to server");
   db = client.db(dbName);
+  //Uncomment if you want to drop user client
+  db.collection('users').drop();
 });
 
 const findDocuments = function(db, collectionName, filter, callback) {
@@ -54,9 +59,33 @@ router.post('/', function(req, res, next) {
   var password = req.body.password;
   console.log(password);
   findDocuments(db, 'users', {'username': username}, (results) => {
-    if (!(results)) {
+    if ( results.length == 0  || !(results) ) {
       insertDocument(db, 'users', {'username': username}, (results) => {
-        res.send('User Just Created');
+        jwt.sign({'username': username}, jwtSecret , { expiresIn: '1d' },(err, token) => {
+          if(err) { console.log(err) }
+          res.send(token);
+        });
+      })
+    }
+    else {
+      res.send('User Already Exists');
+    }
+  })
+});
+
+//Login
+router.post('/', function(req, res, next) {
+  var username = req.body.username;
+  console.log(username);
+  var password = req.body.password;
+  console.log(password);
+  findDocuments(db, 'users', {'username': username}, (results) => {
+    if ( results.length == 0  || !(results) ) {
+      insertDocument(db, 'users', {'username': username}, (results) => {
+        jwt.sign({'username': username}, jwtSecret , { expiresIn: '1d' },(err, token) => {
+          if(err) { console.log(err) }
+          res.send(token);
+        });
       })
     }
     else {
