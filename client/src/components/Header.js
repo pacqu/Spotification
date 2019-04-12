ba1f9e80828df1991703c33f022f0354f896e581
AceroM <miguel.acero81@myhunter.cuny.edu>
2019-04-12 07:20:54 -0400

    nav bar checkpoint

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import classNames from 'classnames';
 import PropTypes from 'prop-types';
 import MediaQuery from 'react-responsive';
+import { slide as Menu } from 'react-burger-menu';
 
 import '../styles/Header.css';
 
@@ -9,25 +10,35 @@ class Header extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      isHover: false
     }
   }
+  showSettings(e) {
+    e.preventDefault();
+  }
   render() {
-    const { name, location } = this.props;
+    const { className, name, location } = this.props;
+    const { isHover } = this.state;
+    const headerStyles = classNames(className, 'nav');
     return (
-      <div className="header">
-        <div className="item logo"> Spotification </div>
-        <div className="item account"> {name} </div>
+      <nav className={headerStyles}>
+        {/* <div className="item logo"> Spotification </div> */}
+        {/* <div className="item account"> {name} </div> */}
         <MediaQuery query="(max-width: 767px)">
-          <ul className="burger-nav">
-            <li href="/">Home</li>
-            <li href="/account">My Acccount</li>
-            <li>Log Out</li>
-          </ul>
+          <Menu>
+            <a id="home" className="menu-item-mobile" href="/">Home</a>
+            <a id="about" className="menu-item-mobile" href="/about">About</a>
+            <a id="contact" className="menu-item-mobile" href="/contact">Contact</a>
+            <a onClick={ this.showSettings } className="menu-item--small" href="">Settings</a>
+          </Menu>
         </MediaQuery>
         <MediaQuery query="(min-width: 768px)">
-        <></>
+          <div className="menu-item-container">
+            <a href="/Home" className="menu-item"> Home </a>
+            <a href="/Account" className="menu-item"> Account </a>
+          </div>
         </MediaQuery>
-      </div>
+      </nav>
     )
   }
 }
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -11,7 +11,7 @@ class Home extends Component {
   constructor(props){
     super(props);
     this.state = {
-      loadingDone: false,
+      loadingDone: true,
       name: "Miguel"
     }
   }
@@ -41,23 +41,27 @@ class Home extends Component {
     if (this.state.loadingDone){
       if (this.state.redirect) return <Redirect to={this.state.redirect} />
       return (
-        <div className="home-container">
+        <main>
           <Header name={name} location="Home"/>
-          <div className="sidebar">
-            this is the sidebar
-            <MediaQuery query="(min-width: 768px)">
-              <img className="avatar" src="https://i.kym-cdn.com/entries/icons/mobile/000/028/861/cover3.jpg" />
-            </MediaQuery>
+          <div className="home-container">
+            <div className="sidebar">
+              this is the sidebar
+              <MediaQuery query="(min-width: 768px)">
+                <img className="avatar" src="https://i.kym-cdn.com/entries/icons/mobile/000/028/861/cover3.jpg" />
+              </MediaQuery>
+            </div>
+            <div className="content">
+              this is the content
+              <button onClick={(e) =>   {
+                  e.preventDefault();
+                  Cookies.remove("cookie");
+                  this.setState({loadingDone: true,redirect: "/login"});
+              }}>
+                log out
+              </button>
+            </div>
           </div>
-          <div className="content">
-            this is the content
-          </div>
-          {/* <button onClick={(e) =>   {
-              e.preventDefault();
-              Cookies.remove("cookie");
-              this.setState({loadingDone: true,redirect: "/login"});
-            }}>log out</button> */}
-        </div>
+        </main>
       )
     }
     else return <div>Loading!</div>
