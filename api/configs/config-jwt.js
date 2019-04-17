cc4be3dbd7b8ad03dfd914190fd493bd4fce1b2c
ilumii <listeven073@gmail.com>
2019-04-16 21:30:52 -0400

    removed files

--- api/configs/config-jwt.js
+++
@@ -1,2 +0,0 @@
-//secret
-module.exports = 'd0nt-l3t-st3lla-handl3-s3cur1ty';
--- api/configs/config-spotify.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  redirectUri :'http://localhost:3000/login',
-  clientId :'41b5ffabfee248e5a3c2fa2a820b46a7',
-  clientSecret :'b3549e5395a54065a2d5c671cba75c54',
-}
