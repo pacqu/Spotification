9122062e02eef9be76d11ccbf8c2f494c6397527
AceroM <miguel.acero81@myhunter.cuny.edu>
2019-04-25 23:29:21 -0400

    checkpoint before adding design systems

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -4,18 +4,23 @@ import Cookies from "js-cookie";
 import { Redirect } from 'react-router-dom';
 import Header from '../components/Header';
 import MediaQuery from 'react-responsive';
-import '../styles/Home.css';
 import { Bar } from 'react-chartjs-2';
+import Chart from '../components/Chart';
+import '../styles/Profile.css';
 
 class Profile extends Component {
 	constructor(props){
     super(props);
     this.state = {
+      loadingDone: false,
+      location: "Profile",
+			name: "",
 			features: [],
 			featuresName: [],
       featuresValue: [],
       topSongs: []
     };
+    this.spotifyData = this.spotifyData.bind(this);
   }
   // Object.entries(res.data[0].listeningData.avgFeatures) -> for each loops thru data
   // Object.entries(res.data[0].listeningData.avgFeatures).filter(item=> item[0] !=="duration_ms") -> filter out ms for now
@@ -74,26 +79,68 @@ class Profile extends Component {
     )
   }
 	
+  componentDidMount(){
+    this.setState({loadingDone: true})
+    if (Cookies.get('cookie')){
+      axios.get('/user/', { headers: {'Authorization' : 'Bearer ' + Cookies.get('cookie')} })
+      .then(res => {
+				console.log(res)
+        console.log(!(res.data[0].spotifyAuth))
+        if(!(res.data[0].spotifyAuth)){
+          this.setState({loadingDone: true, redirect: "/login"})
+        }
+        else{
+          if (res.data[0].listeningData){
+            let array = Object.entries(
+              res.data[0].listeningData.avgFeatures
+            ).filter(item => item[0] !== "duration_ms");
+            this.setState({
+              features: array,
+              featuresName: array.map(item => item[0]),
+              featuresValue: array.map(item => item[1]),
+              topSongs: Object.entries(res.data[0].listeningData.songs).map(
+                item => item[1].name
+              ),
+              loadingDone: true
+            })
+          }
+          else this.spotifyData()
+        }
+      })
+      .catch(err => {
+        console.log(err);
+      });
+    }
+    else{
+      this.setState({loadingDone: true,redirect: "/login"})
+    }
+	}
+	
   render() {
-    const { data, location } = this.props;
-    const { username } = data;
-    return (
-      <main>
-        <Header name={username} location={location} />
-        <div className="home-container">
-          <div className="sidebar">
-            This is the sidebar
-            <MediaQuery query="(min-width: 768px)">
-              <img className="avatar" src="https://i.kym-cdn.com/entries/icons/mobile/000/028/861/cover3.jpg" />
-            </MediaQuery>
+    const { username, location } = this.state;
+    if (this.state.loadingDone){
+      if (this.state.redirect) return <Redirect to={this.state.redirect} />
+      return (
+        <main>
+          <Header name={username} location={location} logout={this.handleLogout}/>
+          <div className="home-container">
+            <div className="sidebar">
+              This is the sidebar
+              <MediaQuery query="(min-width: 768px)">
+                <img className="avatar" src="https://i.kym-cdn.com/entries/icons/mobile/000/028/861/cover3.jpg" />
+              </MediaQuery>
+            </div>
+            <div className="content">
+              <Chart title="hello" className="chart">
+              {this.SongList()}
+                {/* {this.BarGraph()} */}
+              </Chart>
+            </div>
           </div>
-          <div className="content">
-            {this.BarGraph()}
-            {this.SongList()}
-          </div>
-        </div>
-      </main>
-    )
+        </main>
+      )
+    }
+    else return <div>Loading!</div>
   }
 }
 
