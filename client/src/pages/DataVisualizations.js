315c63bce67cbc839354b915b62bf0d2a2706815
ilumii <listeven073@gmail.com>
2019-04-30 12:10:17 -0400

    datavisual.js + double bar

--- client/src/pages/DataVisualizations.js
+++ client/src/pages/DataVisualizations.js
@@ -1,35 +1,190 @@
 import React, { Component } from "react";
 import Header from '../components/Header';
-import MediaQuery from 'react-responsive';
-import '../styles/Home.css';
+import Search from '../components/Search';
+import Button from '../components/Button';
+import SongList from '../components/SongList';
+import MediaListItem from '../components/MediaListItem';
+import axios from "axios";
+import Cookies from "js-cookie";
+import { Bar, Line } from 'react-chartjs-2';
+
+import '../styles/Recommendation.css';
+import '../styles/Layout.css';
 
 class DataVisualizations extends Component {
 	constructor(props){
     super(props);
-	}
-	
+    this.state = {
+      displayChoice: false,
+      currentTab: 'Songs',
+      results: [],
+      recTracks: [],
+      artistResults: [],
+      genres: [],
+      seedTracks: [],
+      seedArtists: [],
+      seedGenres: [],
+      query: '',
+      search: true,
+      visual: false,
+      features: [],
+			featuresName: [],
+      featuresValue: []
+    }
+  }
+
+  handleQuery = e => {
+    e.preventDefault();
+    axios.get(`/search?search=${this.state.query}`, { headers: {'Authorization' : 'Bearer ' + Cookies.get('cookie')} })
+    .then(res => {
+      if (res.data.tracks.items) {
+        this.setState({ results: res.data.tracks.items })
+      }
+      console.log(this.state)
+    })
+    .catch(err => {
+      console.log(err);
+    });
+  }
+
+  handleQueryChange = e => {
+    this.setState({ query: e.target.value })
+    console.log(this.state)
+  }
+
+  handleSongClick = (song) => {
+    let { seedTracks } = this.state;
+    if (seedTracks.length > 1) {
+      alert('Please don\'t add more than 2 songs!');
+    } else {
+      seedTracks.push(song);
+      this.setState({ seedTracks });
+    }
+  }
+
+  removeFromTracks(e, track){
+    let { seedTracks } = this.state
+    seedTracks = seedTracks.filter((seedTrack) => (
+      track.name !== seedTrack.name) || (track.artists[0].name !== seedTrack.artists[0].name) || (track.album.name !== seedTrack.album.name
+    ));
+    this.setState({ seedTracks })
+  }
+
+  handleSubmitVisual = (e) => {
+    e.preventDefault();
+    let trackIds = this.state.seedTracks.map(track => track.id);
+    // axios.get(`/search/song/${trackIds}`, { headers: { Authorization: `Bearer ${Cookies.get("cookie")}`}})
+    // .then(res => {
+    //   console.log(res);
+    // })
+    // .catch(err => {
+    //   console.log(err);
+    // })
+    this.setState({
+      visual:true,
+      search:false
+    })
+  }
+
+  componentDidMount(){
+    this.spotifyData()
+  }
+
+  spotifyData = () => {
+    axios.get("/user/listening-data", {
+      headers: { Authorization: "Bearer " + Cookies.get("cookie") }
+    })
+    .then(res => {
+      let array = Object.entries(
+        res.data[0].listeningData.avgFeatures
+      ).filter(item => item[0] !== "duration_ms");
+      this.setState({
+        features: array,
+        featuresName: array.map(item => item[0]),
+        featuresValue: array.map(item => item[1]),
+        topSongs: Object.entries(res.data[0].listeningData.songs).map(
+          item => item[1].name
+        ),
+        loadingDone: true
+      });
+      console.log(this.state.topSongs);
+      console.log(Object.entries(res.data));
+    });
+  };
+
+  BarGraph = () => {
+    const data = {
+      labels: this.state.featuresName,
+      datasets: [
+        {
+          label: "My Songs", 
+          backgroundColor: 'rgba(255,225,225,.3)',
+          data: this.state.featuresValue
+        },
+        {
+          label: "My Songs",
+          backgroundColor: 'rgba(255,225,225,.7)',
+          data: this.state.featuresValue
+        }
+      ]
+    };
+    return <Bar data={data} />;
+  };
+
   render() {
     const { data, location } = this.props;
+    const { results, displayChoice, recTracks, seedTracks, seedArtists, seedGenres } = this.state;
     const { username } = data;
+    const showRecs = (recTracks.length > 0 && displayChoice);
+    const seedTracksDisplay = seedTracks.map((track, i) => {
+      return (
+        <MediaListItem name={track.name} primaryContext={track.artists[0].name} coverArtUrl={track.album.images[0].url} />
+      )
+    })
+
     return (
       <main>
         <Header name={username} location={location} />
         <div className="home-container">
           <div className="sidebar">
-            This is the sidebar
-            <MediaQuery query="(min-width: 768px)">
-              <img className="avatar" src="https://i.kym-cdn.com/entries/icons/mobile/000/028/861/cover3.jpg" />
-            </MediaQuery>
+            <p>Currently Selected ({seedTracks.length})</p>
+            { seedTracks.length > 0 && (<>
+              { seedTracksDisplay }
+              <Button onClick={(e) => this.handleSubmitVisual(e)}>Data</Button>
+            </>)}
           </div>
+
+        {this.state.search?(   
           <div className="content">
-            <p>stuff</p>
+            <Search
+              handleChange={this.handleQueryChange}
+              handleQuery={this.handleQuery}
+              />
+              { results.length > 0 ? "" : ( <p> Nothing to see here...</p>)}
+              { showRecs ? (<>
+              <h2> Recommendations </h2>
+              <SongList songs={recTracks} />
+              </>) : (
+              <SongList handleClick={this.handleSongClick} songs={results} />
+              )}
           </div>
+        ) : (
+          <div />
+        )}
+
+        {this.state.visual?(
+          <div>
+          {this.BarGraph()}
+          </div>
+        ) : (
+          <div />
+        )}
+
         </div>
       </main>
     )
   }
 }
-
 
 export default DataVisualizations;
 
--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
 import Header from '../components/Header';
 import MediaQuery from 'react-responsive';
-
+import axios from "axios";
+import Cookies from "js-cookie";
 import '../styles/Layout.css';
 import '../styles/Profile.css';
 
@@ -9,17 +10,63 @@ import { Bar } from 'react-chartjs-2';
 import HorizontalBarGraph from '../components/HorizontalBarGraph'
 import Chart from '../components/Chart';
 
-class UserProfile extends Component {
+class Profile extends Component {
 	constructor(props){
     super(props);
     this.state = {
+			features: [],
+			featuresName: [],
+      featuresValue: [],
+      topSongs: []
     };
   }
-
+  // Object.entries(res.data[0].listeningData.avgFeatures) -> for each loops thru data
+  // Object.entries(res.data[0].listeningData.avgFeatures).filter(item=> item[0] !=="duration_ms") -> filter out ms for now
+  // Remove .filter(item=> item[0] !=="duration_ms") => .map(item => item[0]) when graph options fixed.
 	componentDidMount(){
+		const { data }  = this.props;
+		if (!(data.listeningData)) this.spotifyData();
+    else {
+      let array = Object.entries(
+        data.listeningData.avgFeatures
+      ).filter(item => 
+        item[0] !== "duration_ms" 
+        && item[0] !== "tempo" 
+        && item[0] !== "popularity");
+      this.setState({
+        features: array,
+        featuresName: array.map(item => item[0]),
+        featuresValue: array.map(item => item[1]),
+        topSongs: Object.entries(data.listeningData.songs).map(
+          item => item[1].name
+        ),
+        loadingDone: true
+      });
+      console.log(this.state.topSongs);
+      console.log(Object.entries(data));
+    }
 	}
 
   spotifyData = () => {
+    axios.get("/user/listening-data", {
+      headers: { Authorization: "Bearer " + Cookies.get("cookie") }
+    })
+    .then(res => {
+      let array = Object.entries(
+        res.data[0].listeningData.avgFeatures
+      ).filter(item => item[0] !== "duration_ms");
+      this.setState({
+        features: array,
+        featuresName: array.map(item => item[0]),
+        featuresValue: array.map(item => item[1]),
+        topSongs: Object.entries(res.data[0].listeningData.songs).map(
+          item => item[1].name
+        ),
+        loadingDone: true
+      });
+      console.log(this.state.topSongs);
+      console.log(Object.entries(res.data));
+    });
   };
 
   BarGraph = props => {
@@ -34,6 +81,7 @@ class UserProfile extends Component {
           pointStrokeColor: "#fff",
           pointHighlightFill: "#fff",
           pointHighlightStroke: "rgba(220,220,220,1)",
+          backgroundColor: 'rgba(255,225,225,.5)',
           data: this.state.featuresValue
         }
       ]
@@ -41,12 +89,14 @@ class UserProfile extends Component {
     return <Bar data={data} />;
   };
 
-  SongList = (topSongs) => {
-    let songs = topSongs.slice(0,10).map((item =>
+  //maps top 3 songs, change number for more or less songs
+  SongList = () => {
+    let songs = this.state.topSongs.slice(0,3).map((item =>
       <li>{item}</li>
     ))
     return (
       <ul>
+        <h3>My Top Songs</h3>
         <li>{songs}</li>
       </ul>
     )
@@ -54,12 +104,7 @@ class UserProfile extends Component {
 
   render() {
     const { data, location } = this.props;
-    const { username, listeningData } = data;
-
-    const topSongs = Object.entries(listeningData.songs).map(item => [item[1].artists.name, item[1].name])
-    const { sortedGenres } = listeningData;
-    const barData = sortedGenres.map(g => [g.genre, g.count]).slice(0, 7);
-
+    const { username } = data;
     return (
       <main>
         <Header name={username} location={location} />
@@ -71,15 +116,8 @@ class UserProfile extends Component {
             </MediaQuery>
           </div>
           <div className="content">
-            <Chart title="My Top Songs" className="chart">
-              {this.SongList(topSongs)}
-            </Chart>
-            <Chart title="Favorite Genres" className="chart">
-              <HorizontalBarGraph label={"Count"} barData={barData} />
-            </Chart>
-            <Chart title="Bar data">
-              {this.BarGraph()}
-            </Chart>
+            {this.BarGraph()}
+            {this.SongList()}
           </div>
         </div>
       </main>
@@ -87,6 +125,4 @@ class UserProfile extends Component {
   }
 }
 
-
-export default UserProfile;
-
+export default Profile;
